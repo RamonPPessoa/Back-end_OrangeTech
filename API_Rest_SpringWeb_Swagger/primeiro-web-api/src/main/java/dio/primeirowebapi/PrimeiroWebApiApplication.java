b9c821cb5720c41fa6474262853a473a5f80f74b
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-01-28 11:12:59 -0400

    Inciando a criação de uma REST API

---
+++ API_Rest_SpringWeb_Swagger/primeiro-web-api/src/main/java/dio/primeirowebapi/PrimeiroWebApiApplication.java
@@ -0,0 +1,13 @@
+package dio.primeirowebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroWebApiApplication.class, args);
+	}
+
+}
